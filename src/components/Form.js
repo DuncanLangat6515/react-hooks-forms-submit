b70688d8c857676b3c31bb5b26c96c7ad7c278c5
Duncan Cheruiyot Langat <duncan.langat@student.moringaschool.com>
2022-10-07 14:51:45 +0300

    make changes

--- src/components/Form.js
+++ src/components/Form.js
@@ -4,7 +4,9 @@ function Form() {
   const [firstName, setFirstName] = useState("John");
   const [lastName, setLastName] = useState("Henry");
   const [submittedData, setSubmittedData] = useState([]);
+  const [errors, setErrors] = useState([]);
 
+  
   function handleFirstNameChange(event) {
     setFirstName(event.target.value);
   }
@@ -15,11 +17,17 @@ function Form() {
 
   function handleSubmit(event) {
     event.preventDefault();
+     
+  if (firstName.length > 0) {
     const formData = { firstName: firstName, lastName: lastName };
     const dataArray = [...submittedData, formData];
     setSubmittedData(dataArray);
     setFirstName("");
     setLastName("");
+    setErrors([]);
+  } else {
+    setErrors(["First name is required!"]);
+  }
   }
 
   const listOfSubmissions = submittedData.map((data, index) => {
@@ -37,6 +45,13 @@ function Form() {
         <input type="text" onChange={handleLastNameChange} value={lastName} />
         <button type="submit">Submit</button>
       </form>
+      {errors.length > 0
+      ? errors.map((error, index) => (
+          <p key={index} style={{ color: "red" }}>
+            {error}
+          </p>
+        ))
+      : null}
       <h3>Submissions</h3>
       {listOfSubmissions}
     </div>
